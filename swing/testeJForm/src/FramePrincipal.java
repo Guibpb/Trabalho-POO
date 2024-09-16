dbde6052913fdf5c75a1a21db528134ded3be765
Daniel Brito <danielnatanbrito@hotmail.com>
2024-09-16 14:39:11 -0300

    Commits JavaFX

---
+++ swing/testeJForm/src/FramePrincipal.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import net.miginfocom.swing.*;
+/*
+ * Created by JFormDesigner on Wed Sep 04 20:37:52 BRT 2024
+ */
+
+
+
+/**
+ * @author danie
+ */
+public class FramePrincipal extends JFrame {
+    public FramePrincipal() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - unknown
+
+        //======== this ========
+        var contentPane = getContentPane();
+        contentPane.setLayout(new MigLayout(
+            "hidemode 3",
+            // columns
+            "[fill]" +
+            "[fill]",
+            // rows
+            "[]" +
+            "[]" +
+            "[]"));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - unknown
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
