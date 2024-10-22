b2fb07ab4c2a6705d06938f699e710bd5352ea84
daninatan <danielnatanbrito@hotmail.com>
2024-10-21 21:28:40 -0300

    9º commit - Criação do layout da tela de playlist

--- Trabalho/src/main/java/com/example/trabalho/telaPlayerController.java
+++ Trabalho/src/main/java/com/example/trabalho/telaPlayerController.java
@@ -5,15 +5,22 @@ import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+import java.util.Objects;
+
 public class telaPlayerController {
-    private Image btnPauseImage = new Image("file:com/example/trabalho/imagens/btnPause.png");
+    private Image btnPauseImage = new Image(getClass().getResourceAsStream("/com/example/trabalho/imagens/btnPause.png"));
+    private Image btnPlayImage = new Image(getClass().getResourceAsStream("/com/example/trabalho/imagens/btnPlay.png"));
 
     @FXML
     private ImageView imgButton;
 
     @FXML
-    public void btnClick(ActionEvent event) {
-        imgButton.setImage(btnPauseImage);
+    public void btnPlayerClick(ActionEvent event) {
+        if(imgButton.getImage().equals(btnPlayImage)) {
+            imgButton.setImage(btnPauseImage);
+        }else{
+            imgButton.setImage(btnPlayImage);
+        }
     }
 
 }
