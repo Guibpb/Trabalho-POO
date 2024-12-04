16ad63fd07726e42bbc5a065f2be8c4eec833f8b
Guilherme Borges <guilherme.bpb@usp.br>
2024-12-04 16:10:14 -0300

    34º commit - Common Features Javadoc

--- Code/Java/usuarios/CommonFeatures.java
+++ Code/Java/usuarios/CommonFeatures.java
@@ -3,7 +3,22 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Interface que representa ações que qualquer usuário pode realizar.
+ * @author Guilherme
+ * @author Mikhael
+ */
 interface CommonFeatures {
+    /**
+     * Usuário logado segue outro usuário da lista disponível. Dois arquivos são criados, um para seguidores 
+     * do usuário a ser seguido, e um para seguidos do usuários logado. Uma string é formatada com os dados
+     * de cada usuário e salva em um arquivo csv.
+     * Classe RecordUser escreve as informações no arquivo. 
+     * @param userToBeFollowed Nome do usuário a ser seguido.
+     * @param idUserToBeFollowed Id do usuário a ser seguido.
+     * @return Sucesso da ação.
+     * @throws FileNotFoundException Tratamento de erro para arquivo inexistente.
+     */
     default boolean followUser(String userToBeFollowed, String idUserToBeFollowed) throws FileNotFoundException{
         String id = LogIn.user.getId();
         String name = LogIn.user.getName();
@@ -34,6 +49,14 @@ interface CommonFeatures {
         return false;
     }
 
+    /**
+     * O usuário logado realiza ação de parar de seguir um dos seguidores válidos. No arquivo o dado é
+     * substituído por uma String vazia.
+     * @param userToBeUnfollowed Nome do seguidor.
+     * @param idUserToBeUnfollowed Id do seguidor.
+     * @return Sucesso da ação.
+     * @throws FileNotFoundException Tratamento de erro para arquivo inexistente.
+     */
     default boolean unfollowUser(String userToBeUnfollowed, String idUserToBeUnfollowed) throws FileNotFoundException{
         String id = LogIn.user.getId();
         String name = LogIn.user.getName();
@@ -54,6 +77,13 @@ interface CommonFeatures {
         return false;
     }
 
+    /**
+     * Retorna um Array List com os dados dos seguidores do usuário logado, dependendo do identifier retorna
+     * o nome ou o ID. 
+     * @param identifier Inteiro que identifica se os dados serão sobre o nome ou o ID do usuário.
+     * @return ArrayList de String com os dados de todos os seguidores do usuário logado.
+     * @throws FileNotFoundException Tratamento de erro para arquivo inexistente.
+     */
     
     default ArrayList<String> getAllFollowers(int identifier) throws FileNotFoundException{
         String id = LogIn.user.getId();
@@ -76,6 +106,13 @@ interface CommonFeatures {
         return allFollowers;
     }
 
+    /**
+     * Retorna um Array List com os dados de quem o usuário logado está seguindo, dependendo do identifier retorna
+     * o nome ou o ID. 
+     * @param identifier Inteiro que identifica se os dados serão sobre o nome ou o ID do usuário.
+     * @return ArrayList de String com os dados de quem o usuário logado está seguindo.
+     * @throws FileNotFoundException Tratamento de erro para arquivo inexistente.
+     */
     default ArrayList<String> getAllFollowings(int identifier) throws FileNotFoundException{
         String id = LogIn.user.getId();
         
@@ -95,6 +132,12 @@ interface CommonFeatures {
         
         return allFollowings;
     }
+
+    /**
+     * Método que atualiza o arquivo para cada mudança de seguidor/seguido.
+     * @param newData Dados do usuário a ser editado nos seguidores/seguidos.
+     * @throws FileNotFoundException Tratamento de erro para arquivo inexistente.
+     */
 
     default void updateFollowers(String newData) throws FileNotFoundException {
         String id = LogIn.user.getId();
@@ -117,6 +160,10 @@ interface CommonFeatures {
         }
     }
 
+    /**
+     * Método que apaga os dados do usuário logado em todas as pastas.
+     * @throws FileNotFoundException Tratamento de erro para arquivo inexistente.
+     */
     default void deleteSelf() throws FileNotFoundException{
         String id = LogIn.user.getId();
 
@@ -132,6 +179,18 @@ interface CommonFeatures {
         followingsFile.delete();//apaga a pasta de seguidos
     }
 
+    /**
+     * Método que edita os dados do usuário logado no arquivo.
+     * @param newName Novo nome.
+     * @param newEmail Novo e-mail.
+     * @param newPassword Nova senha.
+     * @param newPassword2 Confirmação de nova senha.
+     * @param compareName Booleano que determina se nome será alterado.
+     * @param compareEmail Booleano que determina se E-mail será alterado.
+     * @param comparePassword Booleano que determina se senha será alterada.
+     * @return Retorna número de erro de comparação de contas, para determinar se algum dado é inválido.
+     * @throws FileNotFoundException Tratamento de erro para arquivo inexistente.
+     */
     default int editSelf(String newName, String newEmail, String newPassword, String newPassword2, boolean compareName, boolean compareEmail, boolean comparePassword) throws FileNotFoundException{//função comum
         String tempName = newName;
         String tempEmail = newEmail;
@@ -167,6 +226,15 @@ interface CommonFeatures {
         return 0;
     }
 
+    /**
+     * Cria uma nova playlist, sendo adicionada ao bando de dados das playlists.
+     * @param playlistCSVFileList ArrayList de playlists na qual vai ser adicionada a nova playlist.
+     * @param playlistName Nome da nova playlist.
+     * @param userName Nome do usuário que criou a nova playlist.
+     * @param visibility Visibilidade da nova playlist.
+     * @return Retorna 1 em casos inválidos, retorna 0 por padrão.
+     */
+
     default int createPlaylist(List<List<String>> playlistCSVFileList, String playlistName, String userName, String visibility) {
         if(playlistName.contains(",")){
             return 1;
@@ -181,6 +249,13 @@ interface CommonFeatures {
         return 0;
     }
 
+    /**
+     *Adiciona uma música a uma playlist ja existente, atualiza a playlist no banco de dados das playlists.
+     * @param playlistCSVFileList ArrayList de playlists que vai ser atualizado com a playlist editada.
+     * @param playlistName Nome da playlist a qual vai ser adicionada a nova música.
+     * @param musicID ID da musica que vai ser adicionada à playlist.
+     * @return Retorna 1 em casos inválidos, retorna 0 por padrão.
+     */
     default int addMusicToPlaylist(List<List<String>> playlistCSVFileList, String playlistName, String musicID) {
         for(List<String> playlist : playlistCSVFileList){
             if(playlist.getFirst().equals(playlistName)){
@@ -196,6 +271,11 @@ interface CommonFeatures {
         return 0;
     }
 
+    /**
+     * Deleta uma playlist do banco de dados, atualiza a playlist no banco de dados das playlists.
+     * @param playlistCSVFileList ArrayList de playlists do qual vai ser deletado a playlist.
+     * @param playlistName Nome da playlist que vai ser deletada.
+     */
     default void deletePlaylist(List<List<String>> playlistCSVFileList, String playlistName) {
         for(List<String> playlist : playlistCSVFileList){
             if(playlist.getFirst().equals(playlistName)){
@@ -206,6 +286,14 @@ interface CommonFeatures {
         PlaylistDatabase.updatePlaylistCSVFile(playlistCSVFileList);
     }
 
+    /**
+     * Edita os dados de uma playlist, atualiza a playlist no banco de dados das playlists.
+     * @param playlistCSVFileList ArrayList de playlists que vai ser atualizado com a playlist editada.
+     * @param oldPlaylistName Nome atual da playlist.
+     * @param newPlaylistName Novo nome da playlist.
+     * @param newVisibility Nova visibilidade da playlist.
+     * @return Retorna 1 em casos inválidos, retorna 0 por padrão.
+     */
     default int editPlaylist(List<List<String>> playlistCSVFileList, String oldPlaylistName, String newPlaylistName, String newVisibility) {
         if(newPlaylistName.contains(",")){
             return 1;
@@ -222,6 +310,13 @@ interface CommonFeatures {
         return 0;
     }
 
+    /**
+     * Remove uma música de uma playlist ja existente, atualiza a playlist no banco de dados das playlists.
+     * @param playlistCSVFileList ArrayList de playlists que vai ser atualizado com a playlist editada.
+     * @param playlistName Nome da playlist a qual vai ser removida uma música.
+     * @param musicID ID da musica que vai ser removida da playlist.
+     */
+
     default void removeMusicFromPlaylist(List<List<String>> playlistCSVFileList, String playlistName, String musicID){
         for(int i=1; i<playlistCSVFileList.size(); i++){
             if(playlistCSVFileList.get(i).getFirst().equals(playlistName)){
@@ -235,6 +330,12 @@ interface CommonFeatures {
         }
         PlaylistDatabase.updatePlaylistCSVFile(playlistCSVFileList);
     }
+
+    /**
+     * Recebe uma música da Lista de músicas por meio do ID, incrementa a visualização dessa música.
+     * @param musicCSVFileList Lista de músicas da qual sera retirada uma música específica para incrementar a visualização.
+     * @param musicID ID específico da música que foi visualizada e incrementada por 1 as visualizações.
+     */
 
     default void viewCounter(String musicID){
         List<String[]> musicCSVFileList = MusicDatabase.getMusicCSVFile();
