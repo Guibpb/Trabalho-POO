90e9bc51bbba426b3cd143af22a91012d408a250
Guilherme Borges <guilherme.bpb@usp.br>
2024-12-04 16:22:38 -0300

    35º commit - AdmUser Javadoc

--- Code/Java/usuarios/AdmUser.java
+++ Code/Java/usuarios/AdmUser.java
@@ -1,19 +1,45 @@
+/**
+ * Classe que representa o Gerente do aplicativo.
+ * Herda da classe abstrata User.
+ * Implementa CommomFeatures por herança, AdmFeatures e ArtistFeatures diretamente.
+ * É utilizada como objeto no usuário logado, se tal usuário for Gerente do aplicativo.
+ */
+
 public class AdmUser extends User implements AdmFeatures, ArtistFeatures{
+    /**
+     * Construtor.
+     * @param id ID.
+     * @param name Nome.
+     * @param email E-mail.
+     * @param password Senha.
+     */ 
     public AdmUser(String id, String name, String email, String password){
         super(id, name, email, password);
     }
 
+    /**
+     * Método herdado de User.
+     * Retorna o cargo.
+     */
     @Override
     public String getRole(){
         return ROLE;
     }
 
+    /**
+     * Método herdado de User.
+     * Retorna os dados em formatação de String com vírgula para CSV.
+     */
     @Override
     public String getFormatData(){
         String data = super.getFormatData() + ROLE;
         return data;
     }
 
+    /**
+     * Método herdado de User.
+     * Retorna os dados por vetores.
+     */
     @Override
     public String[] getData(){
         String[] origin = super.getData();
