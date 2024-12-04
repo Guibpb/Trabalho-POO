457c1718b5f88a85fba171150a5718befa975acb
Guilherme Borges <guilherme.bpb@usp.br>
2024-12-04 17:02:22 -0300

    38º commit - LogIn Javadoc

--- Code/Java/usuarios/LogIn.java
+++ Code/Java/usuarios/LogIn.java
@@ -1,10 +1,26 @@
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
+/**
+ * A classe LogIn fornece métodos para autenticação de usuários com base em um banco de dados.
+ * @author Guilherme
+ */
+
 public class LogIn {
     public static User user;
     public static AdmUser admUser;
     public static ArtistUser artistUser;
+
+    /**
+     * Tenta fazer login com o usuário e senha fornecidos.
+     * Utiliza um Array List de array de String como matriz para receber dados de todos os usuários,
+     * itera nessa matriz para realizar o login com os dados.
+     * 
+     * @param userInput O nome de usuário ou email fornecido pelo usuário.
+     * @param passwordInput A senha fornecida pelo usuário.
+     * @return true se o login for bem-sucedido, false caso contrário.
+     * @throws FileNotFoundException Se o arquivo do banco de dados não for encontrado.
+     */
 
     public static boolean logIn(String userInput, String passwordInput) throws FileNotFoundException{ 
         ArrayList<String[]> matrixInfo = FileInfo.getMatrixInfo("Database/Banco.csv");
@@ -21,6 +37,15 @@ public class LogIn {
         }
         return false;
     }
+
+    /**
+     * Define um objeto com o tipo de usuário com base nas informações fornecidas.
+     * user é o objeto genérico, admUser e artistUser são objetos utilizados em 
+     * métodos específicos.
+     *
+     * @param userInfo Um array de strings contendo as informações do usuário.
+     * @return true se o usuário foi definido com sucesso, false caso contrário.
+     */
 
     public static boolean defUser(String [] userInfo){
         boolean success = false;
